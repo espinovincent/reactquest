3b304fa71f222bd84f9ee9ae561332bf46f71f8b
espino vincent <tantalas95@gmail.com>
2024-01-08 16:30:43 +0100

    challenge finished

---
+++ my-react-app/src/components/PokemonCard.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+function PokemonCard() {
+    return (
+        <figure>
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"></img>
+            <figcaption>Bulbasaur</figcaption>
+
+        </figure>
+
+    );
+  }
+  
+  // eslint-disable-next-line no-undef
+  export default PokemonCard;
