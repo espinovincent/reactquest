1bdc1fcc06bbc82d258e57bfcfc0653caf3848b8
espino vincent <tantalas95@gmail.com>
2024-01-10 11:51:48 +0100

    finish 4

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,10 +1,27 @@
-import PokemonCard from "./components/PokemonCard"
+import './App.css'
+import PokemonCard from './components/PokemonCard';
+
+const pokemonList = [
+
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+
+  {
+    name: "mew",
+  },
+
+];
 
 function App() {
+  let pokemon = pokemonList[0]
   return (
     <div>
-      <PokemonCard/>
+      <PokemonCard pokemon={pokemon} />
     </div>
   );
+
 }
 export default App
--- my-react-app/src/components/PokemonCard.jsx
+++ my-react-app/src/components/PokemonCard.jsx
@@ -1,26 +1,10 @@
-// eslint-disable-next-line no-unused-vars
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
-
-
-  function PokemonCard() {
-    let pokemon = pokemonList[0];
-    return (
-        <figure>
-            {pokemon.imgSrc ? (<img src={pokemon.imgSrc} alt={pokemon.name} />) : (<p>?????</p>)}
-            <figcaption>{pokemon.name}</figcaption>
-        </figure>
-    );
-}
-  
-  // eslint-disable-next-line no-undef
-  export default PokemonCard
-  ;
+function PokemonCard(props) {
+  let { pokemon } = props
+  return (
+      <figure>
+          {pokemon.imgSrc ? (<img src={pokemon.imgSrc} alt={pokemon.name} />) : (<p>?????</p>)}
+          <figcaption>{pokemon.name}</figcaption>
+      </figure>
+  );
+};
+export default PokemonCard
