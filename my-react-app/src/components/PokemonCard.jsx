aaed9b591bd166b0107f5f2b9dbe80b7981987b0
espino vincent <tantalas95@gmail.com>
2024-01-10 12:13:13 +0100

    finish 5

--- my-react-app/src/components/PokemonCard.jsx
+++ my-react-app/src/components/PokemonCard.jsx
@@ -1,10 +1,20 @@
+import PropTypes from "prop-types";
+
 function PokemonCard(props) {
-  let { pokemon } = props
-  return (
-      <figure>
-          {pokemon.imgSrc ? (<img src={pokemon.imgSrc} alt={pokemon.name} />) : (<p>?????</p>)}
-          <figcaption>{pokemon.name}</figcaption>
-      </figure>
-  );
+    let { pokemon } = props
+    return (
+        <figure>
+            {pokemon.imgSrc ? (<img src={pokemon.imgSrc} alt={pokemon.name} />) : (<p>?????</p>)}
+            <figcaption>{pokemon.name}</figcaption>
+        </figure>
+    );
 };
+
+PokemonCard.propTypes = {
+    pokemon: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        imgSrc: PropTypes.string,
+    }).isRequired,
+};
+
 export default PokemonCard
